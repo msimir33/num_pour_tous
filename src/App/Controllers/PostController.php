7b9be89f353e0c25837da597e0d640955363ee42
Nour-eddine <fattoumne@gmail.com>
2022-06-23 16:23:35 +0200

    20220623_test_created_post

--- src/App/Controllers/PostController.php
+++ src/App/Controllers/PostController.php
@@ -12,42 +12,44 @@ class PostController extends AbstractController
     {
         $model = new PostModel();
         $posts = $model->findAll();
-        
+
         $this->display('posts/index', [
-            'posts' => $posts    
+            'posts' => $posts
         ]);
     }
-    
+
     public function show(): void
     {
         $id = $_GET['id'];
         $model = new PostModel();
         $post = $model->find($id);
-        
+
         if ($post === null) {
             throw new NotFoundException("L'annonce n'existe pas");
         }
-        
+
         $this->display('posts/show', [
-            'post' => $post    
+            'post' => $post
         ]);
     }
-    
+
     public function create(): void
     {
         $this->display('posts/create');
     }
-    
+
     public function store(): void
     {
+        if (!auth()->isAuthenticated()) {
+            $this->redirect('/login');
+        }
         $model = new PostModel();
-        
-        $id = $model->create([
+        $model->create([
             'post_title' => $_POST['post_title'],
             'post_content' => $_POST['post_content'],
-            'post_creation_date' => $_POST['post_creation_date']
+            'post_creation_date' => $_POST['post_creation_date'],
         ]);
-        
+
         $this->redirect('/posts');
     }
 }
--- src/App/Models/PostModel.php
+++ src/App/Models/PostModel.php
@@ -9,43 +9,64 @@ class PostModel extends AbstractModel
     public function findAll(): array
     {
         return $this->db->getResults(
-            'SELECT post_id, post_title, post_content, post_creation_date 
+            'SELECT post_id, post_title, post_content, post_creation_date
             FROM posts
             ORDER BY post_creation_date'
         );
     }
-    
+
     public function find(int $id): ?array
     {
         $results = $this->db->getResults(
-            'SELECT post_id, post_title, post_content, post_creation_date 
+            'SELECT post_id, post_title, post_content, post_creation_date
             FROM posts
             WHERE post_id = :post_id', [
-                'post_id' => $post_id    
-            ]   
+                'post_id' => $post_id
+            ]
         );
-        
+
         if (empty($results)) {
             return null;
         }
-        
+
         return $results[0];
     }
-    
+
+    public function getUserId(int $id): ?int
+    {
+        $results = $this->db->getResults(
+            'SELECT user_id
+            FROM users
+            WHERE user_id = :user_id',
+            [
+                'user_id' => $id
+            ]
+        );
+
+        if (empty($results)) {
+            return null;
+        }
+
+        return $results[0]['user_id'];
+    }
+
     public function create(array $data): ?int
     {
+        $userId = $this->getUserId(auth()->isAuthenticated());
         $result = $this->db->execute(
             'INSERT INTO posts (post_title, post_content, post_creation_date) VALUES (:post_title, :post_content, :post_creation_date)', [
                 'post_title' => $data['post_title'],
                 'post_content' => $data['post_content'],
-                'post_creation_date' => $data['post_creation_date']
+                'post_creation_date' => $data['post_creation_date'],
+                'user_id' => $userId
             ]
         );
-        
+        die($result);
+
         if ($result === false) {
             return null;
         }
-        
+
         return (int)$result;
     }
 }
